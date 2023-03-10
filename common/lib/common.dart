b705b94214b501dfb1d3c47551a2019772ac0c62
kovarny <sivochkin@gmail.com>
2023-03-10 18:25:29 +1100

    update

--- common/lib/common.dart
+++ common/lib/common.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 typedef DS = DocumentSnapshot<Map<String, dynamic>>;
+typedef QS = QuerySnapshot<Map<String, dynamic>>;
 typedef DR = DocumentReference<Map<String, dynamic>>;
 typedef CR = CollectionReference<Map<String, dynamic>>;
 

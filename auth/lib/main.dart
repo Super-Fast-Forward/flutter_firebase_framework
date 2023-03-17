82a055e422b6587a93b6f2958ce29d89bb7b779a
kovarny <sivochkin@gmail.com>
2023-03-17 14:20:29 +1100

    clean-up

--- auth/lib/main.dart
+++ auth/lib/main.dart
@@ -1,84 +1,17 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'firebase_options.dart';
-import 'generic_state_notifier.dart';
 
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
+class _GenericStateNotifier<V> extends StateNotifier<V> {
+  _GenericStateNotifier(V d) : super(d);
 
-//   await Firebase.initializeApp(
-//     options: DefaultFirebaseOptions.currentPlatform,
-//   );
+  set value(V v) {
+    state = v;
+  }
 
-//   runApp(ProviderScope(child: MainApp()));
-// }
+  V get value => state;
+}
 
-// class MainApp extends ConsumerWidget {
-//   const MainApp({
-//     Key? key,
-//   }) : super(key: key);
+final isLoggedIn = StateNotifierProvider<_GenericStateNotifier<bool>, bool>(
+    (ref) => _GenericStateNotifier<bool>(false));
 
-//   @override
-//   Widget build(BuildContext context, WidgetRef ref) {
-//     return MaterialApp(
-//       title: 'JOB SEARCH NINJA',
-//       home: TheApp(),
-//     );
-//   }
-// }
-
-final isLoggedIn = StateNotifierProvider<GenericStateNotifier<bool>, bool>(
-    (ref) => GenericStateNotifier<bool>(false));
-
-final isLoading = StateNotifierProvider<GenericStateNotifier<bool>, bool>(
-    (ref) => GenericStateNotifier<bool>(false));
-
-// class TheApp extends ConsumerStatefulWidget {
-//   const TheApp({Key? key}) : super(key: key);
-//   @override
-//   TheAppState createState() => TheAppState();
-// }
-
-// class TheAppState extends ConsumerState<TheApp> {
-//   //bool isLoading = false;
-//   @override
-//   void initState() {
-//     super.initState();
-//     ref.read(isLoading.notifier).value = true;
-//     FirebaseAuth.instance.authStateChanges().listen((User? user) {
-//       if (user == null) {
-//         ref.read(isLoggedIn.notifier).value = false;
-//         ref.read(isLoading.notifier).value = false;
-//       } else {
-//         ref.read(isLoggedIn.notifier).value = true;
-//         ref.read(isLoading.notifier).value = false;
-//       }
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     if (ref.watch(isLoading)) {
-//       return Center(
-//         child: Container(
-//           alignment: Alignment(0.0, 0.0),
-//           child: CircularProgressIndicator(),
-//         ),
-//       );
-//     } else {
-//       return Scaffold(
-//           body: ref.watch(isLoggedIn) == false
-//               ? LoginScreen('login', 'test', {
-//                   "loginGitHub": false,
-//                   "loginGoogle": true,
-//                   "loginEmail": false,
-//                   "loginSSO": false,
-//                   "loginAnonymous": true,
-//                   "signupOption": false,
-//                 })
-//               : Text('Logged in!'));
-//     }
-//   }
-// }
+final isLoading = StateNotifierProvider<_GenericStateNotifier<bool>, bool>(
+    (ref) => _GenericStateNotifier<bool>(false));

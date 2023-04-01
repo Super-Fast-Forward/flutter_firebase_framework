2766d4ea3678941bfd5b01692eb4ca2b2e1c8952
kovarny <sivochkin@gmail.com>
2023-04-02 07:53:00 +1000

    disable isLoggedIn

--- auth/lib/main.dart
+++ auth/lib/main.dart
@@ -1,17 +1,17 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class _GenericStateNotifier<V> extends StateNotifier<V> {
-  _GenericStateNotifier(V d) : super(d);
+// class _GenericStateNotifier<V> extends StateNotifier<V> {
+//   _GenericStateNotifier(V d) : super(d);
 
-  set value(V v) {
-    state = v;
-  }
+//   set value(V v) {
+//     state = v;
+//   }
 
-  V get value => state;
-}
+//   V get value => state;
+// }
 
-final isLoggedIn = StateNotifierProvider<_GenericStateNotifier<bool>, bool>(
-    (ref) => _GenericStateNotifier<bool>(false));
+// final isLoggedIn = StateNotifierProvider<_GenericStateNotifier<bool>, bool>(
+//     (ref) => _GenericStateNotifier<bool>(false));
 
-final isLoading = StateNotifierProvider<_GenericStateNotifier<bool>, bool>(
-    (ref) => _GenericStateNotifier<bool>(false));
+// final isLoading = StateNotifierProvider<_GenericStateNotifier<bool>, bool>(
+//     (ref) => _GenericStateNotifier<bool>(false));

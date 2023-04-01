e5a5d16cf46a992d3e3930d47e76f18a9ce220f0
kovarny <sivochkin@gmail.com>
2023-04-02 07:37:49 +1000

    customize mode theme

--- providers/lib/theme_mode.dart
+++ providers/lib/theme_mode.dart
@@ -6,35 +6,32 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class ThemeModeStateNotifier extends StateNotifier<bool> {
-  late DocumentReference Function() saveDocRefBuilder;
+  late void Function(bool) saveBuilder;
+  late Future<bool> Function() getBuilder;
 
   final FirebaseAuth auth = FirebaseAuth.instance;
   final dbInstance = FirebaseFirestore.instance;
 
   ThemeModeStateNotifier(bool loginState) : super(false) {
-    this.saveDocRefBuilder =
-        () => dbInstance.collection('user').doc(auth.currentUser!.uid);
+    this.saveBuilder = ThemeModeConfig.saveBuilder;
+    this.getBuilder = ThemeModeConfig.getBuilder;
     if (loginState == true && auth.currentUser != null) {
-      dbInstance
-          .collection('user')
-          .doc(auth.currentUser!.uid)
-          .get()
-          .then((value) {
-        String theme = value['themeMode'];
-        bool isDark = theme == 'light' ? false : true;
-        state = isDark;
-      });
+      getBuilder().then((v) => state = v);
+      // dbInstance.collection('user').doc(auth.currentUser!.uid)
+
+      //     .get()
+      //     .then((value) {
+      //   String theme = value['themeMode'];
+      //   bool isDark = theme == 'light' ? false : true;
+      //   state = isDark;
+      // });
     }
   }
   void changeTheme() {
     state = !state;
 
-    String themeMode = state == false ? 'light' : 'dark';
     if (auth.currentUser != null) {
-      dbInstance
-          .collection('user')
-          .doc(auth.currentUser!.uid)
-          .set({'themeMode': themeMode});
+      saveBuilder(state);
     }
   }
 }
@@ -43,3 +40,25 @@ final themeModeSNP = StateNotifierProvider<ThemeModeStateNotifier, bool>((ref) {
   bool loginState = ref.watch(isLoggedIn);
   return ThemeModeStateNotifier(loginState);
 }, dependencies: [isLoggedIn]);
+
+class ThemeModeConfig {
+  static bool enableSave = true;
+  static Function(bool themeMode) saveBuilder = (themeMode) => FirebaseFirestore
+      .instance
+      .collection('user')
+      .doc(FirebaseAuth.instance.currentUser!.uid)
+      .set({'themeMode': themeMode == false ? 'light' : 'dark'});
+  static Future<bool> Function() getBuilder = () async {
+    final theme = (await FirebaseFirestore.instance
+            .collection('user')
+            .doc(FirebaseAuth.instance.currentUser!.uid)
+            .get())
+        .data()!['themeMode'];
+    return theme == 'light' ? false : true;
+    //     .then((value) {
+    //   String theme = value['themeMode'];
+    //   bool isDark = theme == 'light' ? false : true;
+    //   state = isDark;
+    // });
+  };
+}

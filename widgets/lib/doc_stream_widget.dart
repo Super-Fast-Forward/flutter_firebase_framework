4a06515b555ac444cf27b4ebb9854bae93c26a08
kovarny <sivochkin@gmail.com>
2023-04-05 14:50:33 +1000

    doc

--- widgets/lib/doc_stream_widget.dart
+++ widgets/lib/doc_stream_widget.dart
@@ -2,6 +2,23 @@ import 'package:common/common.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+///
+/// A widget that builds itself based on the latest snapshot of interaction
+/// with a [Stream].
+///
+/// [docStreamProvider]: The stream provider to listen to.
+/// [builder]: Called every time the [docStreamProvider] emits a new item to build
+/// the widget based on the document received from Firestore.
+///
+/// Example:
+/// ```dart
+/// Widget build(BuildContext context, WidgetRef ref) {
+///  return
+///   DocStreamWidget(
+///    docSP('test_collection/test_doc'),
+///   (context, doc) => Text(doc.data()['text'])),
+/// );
+///
 class DocStreamWidget extends ConsumerWidget {
   final Widget Function(BuildContext context, DS doc) builder;
   final AutoDisposeStreamProvider<DS> docStreamProvider;

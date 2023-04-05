f317e67fdefffba7ece75b82e0ad91b317a0b16f
kovarny <sivochkin@gmail.com>
2023-04-06 09:48:21 +1000

    TABS

--- widgets/lib/app_bar/app_bar.dart
+++ widgets/lib/app_bar/app_bar.dart
@@ -4,26 +4,33 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:widgets/app_bar/theme_switch.dart';
 
 class CustomAppBar extends ConsumerWidget implements PreferredSizeWidget {
-  final Widget title;
+  final Widget? leading;
+  final Widget? title;
+  final List<String>? tabs;
   //all other properties of AppBar widget can be added here
   final Widget userAvatar;
   final Widget themeButton;
   final bool showUserAvatar;
   final bool showThemeButton;
+  final ValueChanged<String>? onTabSelected;
 
   CustomAppBar({
     Key? key,
-    this.title = const Text(''),
+    this.leading,
+    this.title,
+    this.tabs,
     this.showUserAvatar = true,
     this.showThemeButton = true,
     this.userAvatar = const CurrentUserAvatarExtended(),
     this.themeButton = const ThemeSwitch(),
+    this.onTabSelected,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return AppBar(
-      title: title,
+      leading: leading,
+      title: title == null ? _buildTabBar(context, ref) : title,
       actions: [
         if (showUserAvatar) userAvatar,
         if (showThemeButton) themeButton
@@ -33,4 +40,32 @@ class CustomAppBar extends ConsumerWidget implements PreferredSizeWidget {
 
   @override
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
+  Widget _buildTabBar(BuildContext context, WidgetRef ref) {
+    return tabs == null
+        ? Container()
+        : Align(
+            child: SizedBox(
+                width: 800,
+                child: TabBar(
+                  tabs: tabs!
+                      .map((t) => Tab(
+                          iconMargin: EdgeInsets.all(0),
+                          child:
+                              // GestureDetector(
+                              //     behavior: HitTestBehavior.translucent,
+                              //onTap: () => navigatePage(text, context),
+                              //child:
+                              Text(
+                            t.toUpperCase(),
+                            overflow: TextOverflow.fade,
+                            softWrap: false,
+                            style: Theme.of(context).textTheme.titleSmall,
+                          )))
+                      .toList(),
+                  onTap: (index) {
+                    Navigator.of(context).pushNamed(tabs![index]);
+                  },
+                )));
+  }
 }

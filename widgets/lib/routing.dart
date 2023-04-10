e8d6a937e2ed7ba6790e1fdd89a97c477c6e9009
kovarny <sivochkin@gmail.com>
2023-04-10 15:06:18 +1000

    routing

--- widgets/lib/routing.dart
+++ widgets/lib/routing.dart
@@ -1,128 +1,182 @@
 import 'package:flutter/material.dart';
 
-PageRouteBuilder Function(RouteSettings settings) generateRoutes(
-        Map<String,
-                Widget Function(BuildContext context, RouteSettings settings)>
-            routes) =>
-    (RouteSettings settings) {
-      // print('showing ${settings.name}');
-
-      return PageRouteBuilder(
-        settings: settings,
-        pageBuilder: (BuildContext context, Animation<double> animation,
-            Animation<double> secondaryAnimation) {
-          if (routes.containsKey(settings.name)) {
-            return routes[settings.name]!(context, settings);
-          }
-          //return routes[settings.name]!(context, settings);
-          throw Exception('Invalid route: ${settings.name}');
-        },
-        transitionsBuilder: (BuildContext context, Animation<double> animation,
-            Animation<double> secondaryAnimation, Widget child) {
-          return child;
-        },
-      );
-    };
-
-PageRouteBuilder Function(RouteSettings settings) generateRoutesWithRegexp(
-        Map<String,
-                Widget Function(BuildContext context, RouteSettings settings)>
-            routes) =>
-    (RouteSettings settings) {
-      // print('showing ${settings.name}');
-
-      for (var route in routes.keys) {
-        final RegExp pattern = RegExp(route);
-        final match = pattern.firstMatch(settings.name!);
-        if (match != null) {
-          final String execId = match.group(1)!;
-          final String docId = match.group(2)!;
-          // return MaterialPageRoute(
-          //   builder: (BuildContext context) => routes[route]!(context, settings),
-          //   settings: settings,
-          // );
-
-          return PageRouteBuilder(
-            settings: settings,
-            pageBuilder: (BuildContext context, Animation<double> animation,
-                Animation<double> secondaryAnimation) {
-              if (routes.containsKey(settings.name)) {
-                //return routes[settings.name]!(context, settings);
-                return routes[route]!(
-                    context,
-                    RouteSettings(
-                        name: settings.name,
-                        arguments: {'execId': execId, 'docId': docId}));
-              }
-              //return routes[settings.name]!(context, settings);
-              throw Exception('Invalid route: ${settings.name}');
-            },
-            transitionsBuilder: (BuildContext context,
-                Animation<double> animation,
-                Animation<double> secondaryAnimation,
-                Widget child) {
-              return child;
-            },
-          );
-        }
-      }
-      throw Exception('Invalid route: ${settings.name}');
-    };
-
 String _routePattern(String route) {
-  return route.replaceAllMapped(RegExp(r'\{(\w+)\}'), (match) => r'(\w+)');
+  print('_routePattern from: ${route}');
+  final r = route.replaceAllMapped(RegExp(r'\{(\w+)\}'), (match) => r'(\w+)');
+  print('_routePattern to: ${r}');
+  return r;
 }
 
-RouteFactory generateRoutes2(
+RouteFactory generateRoutes(
     Map<String, Widget Function(BuildContext, RouteSettings)> routeBuilders) {
   return (RouteSettings settings) {
+    print('parse route: ${settings.name}');
     for (final route in routeBuilders.keys) {
+      print('  route: ${route}');
       final RegExp pattern = RegExp('^${_routePattern(route)}\$');
+      print('  pattern: ${pattern}');
       final match = pattern.firstMatch(settings.name!);
+      print('  match: ${match}');
       if (match != null) {
         final builder = routeBuilders[route]!;
-        return MaterialPageRoute(
-          builder: (BuildContext context) => builder(context, settings),
-          settings: settings,
+        final groupCounts =
+            List.generate(match.groupCount, (index) => index + 1);
+        final groups = match.groups(groupCounts);
+        print('  create route with ${groups}}');
+        return PageRouteBuilder(
+          pageBuilder: (context, animation1, animation2) => builder(
+              context, RouteSettings(name: settings.name, arguments: groups)),
+          settings: RouteSettings(name: settings.name, arguments: groups),
+          transitionDuration: Duration.zero,
+          // transitionsBuilder: (context, animation1, animation2, child) => child,
         );
+        // MaterialPageRoute(
+        //     builder: (BuildContext context) => builder(
+        //         context, RouteSettings(name: settings.name, arguments: groups)),
+        //     settings: RouteSettings(name: settings.name, arguments: groups)
+        //     //settings,
+        //     );
+
+        // return MaterialPageRoute(
+        //   builder: (BuildContext context) => builder(context, settings),
+        //   settings: settings,
+        // );
       }
     }
-
+    print('no match found!');
+    throw Exception('Invalid route: ${settings.name}');
     // If no match is found, return an error page.
-    return MaterialPageRoute(
-      builder: (BuildContext context) => ErrorPage(),
-      settings: settings,
-    );
+    // return MaterialPageRoute(
+    //   builder: (BuildContext context) => ErrorWidget('error'), //ErrorPage(),
+    //   settings: settings,
+    // );
   };
 }
 
 
-// PageRouteBuilder generateRoute(RouteSettings settings) {
-//   WidgetBuilder builder;
-//   // print('showing ${settings.name}');
-//   switch (settings.name) {
-//     case '/providers':
-//       builder = (BuildContext context) => ProvidersPage();
-//       break;
-//     case '/auth':
-//       builder = (BuildContext context) => AuthPage();
-//       break;
-//     case '/widgets':
-//       builder = (BuildContext context) => WidgetsPage();
-//       break;
-//     default:
-//       throw Exception('Invalid route: ${settings.name}');
-//   }
+// PageRouteBuilder Function(RouteSettings settings) generateRoutes(
+//         Map<String,
+//                 Widget Function(BuildContext context, RouteSettings settings)>
+//             routes) =>
+//     (RouteSettings settings) {
+//       // print('showing ${settings.name}');
 
-//   return PageRouteBuilder(
-//     settings: settings,
-//     pageBuilder: (BuildContext context, Animation<double> animation,
-//         Animation<double> secondaryAnimation) {
-//       return builder(context);
-//     },
-//     transitionsBuilder: (BuildContext context, Animation<double> animation,
-//         Animation<double> secondaryAnimation, Widget child) {
-//       return child;
-//     },
-//   );
+//       return PageRouteBuilder(
+//         settings: settings,
+//         pageBuilder: (BuildContext context, Animation<double> animation,
+//             Animation<double> secondaryAnimation) {
+//           if (routes.containsKey(settings.name)) {
+//             return routes[settings.name]!(context, settings);
+//           }
+//           //return routes[settings.name]!(context, settings);
+//           throw Exception('Invalid route: ${settings.name}');
+//         },
+//         transitionsBuilder: (BuildContext context, Animation<double> animation,
+//             Animation<double> secondaryAnimation, Widget child) {
+//           return child;
+//         },
+//       );
+//     };
+
+// PageRouteBuilder Function(RouteSettings settings) generateRoutesWithRegexp(
+//         Map<String,
+//                 Widget Function(BuildContext context, RouteSettings settings)>
+//             routes) =>
+//     (RouteSettings settings) {
+//       // print('showing ${settings.name}');
+
+//       for (var route in routes.keys) {
+//         final RegExp pattern = RegExp(route);
+//         final match = pattern.firstMatch(settings.name!);
+//         if (match != null) {
+//           final String execId = match.group(1)!;
+//           final String docId = match.group(2)!;
+//           // return MaterialPageRoute(
+//           //   builder: (BuildContext context) => routes[route]!(context, settings),
+//           //   settings: settings,
+//           // );
+
+//           return PageRouteBuilder(
+//             settings: settings,
+//             pageBuilder: (BuildContext context, Animation<double> animation,
+//                 Animation<double> secondaryAnimation) {
+//               if (routes.containsKey(settings.name)) {
+//                 //return routes[settings.name]!(context, settings);
+//                 return routes[route]!(
+//                     context,
+//                     RouteSettings(
+//                         name: settings.name,
+//                         arguments: {'execId': execId, 'docId': docId}));
+//               }
+//               //return routes[settings.name]!(context, settings);
+//               throw Exception('Invalid route: ${settings.name}');
+//             },
+//             transitionsBuilder: (BuildContext context,
+//                 Animation<double> animation,
+//                 Animation<double> secondaryAnimation,
+//                 Widget child) {
+//               return child;
+//             },
+//           );
+//         }
+//       }
+//       throw Exception('Invalid route: ${settings.name}');
+//     };
+
+// String _routePattern(String route) {
+//   return route.replaceAllMapped(RegExp(r'\{(\w+)\}'), (match) => r'(\w+)');
 // }
+
+// RouteFactory generateRoutes2(
+//     Map<String, Widget Function(BuildContext, RouteSettings)> routeBuilders) {
+//   return (RouteSettings settings) {
+//     for (final route in routeBuilders.keys) {
+//       final RegExp pattern = RegExp('^${_routePattern(route)}\$');
+//       final match = pattern.firstMatch(settings.name!);
+//       if (match != null) {
+//         final builder = routeBuilders[route]!;
+//         return MaterialPageRoute(
+//           builder: (BuildContext context) => builder(context, settings),
+//           settings: settings,
+//         );
+//       }
+//     }
+
+//     // If no match is found, return an error page.
+//     return MaterialPageRoute(
+//       builder: (BuildContext context) => ErrorPage(),
+//       settings: settings,
+//     );
+//   };
+// }
+
+
+// // PageRouteBuilder generateRoute(RouteSettings settings) {
+// //   WidgetBuilder builder;
+// //   // print('showing ${settings.name}');
+// //   switch (settings.name) {
+// //     case '/providers':
+// //       builder = (BuildContext context) => ProvidersPage();
+// //       break;
+// //     case '/auth':
+// //       builder = (BuildContext context) => AuthPage();
+// //       break;
+// //     case '/widgets':
+// //       builder = (BuildContext context) => WidgetsPage();
+// //       break;
+// //     default:
+// //       throw Exception('Invalid route: ${settings.name}');
+// //   }
+
+// //   return PageRouteBuilder(
+// //     settings: settings,
+// //     pageBuilder: (BuildContext context, Animation<double> animation,
+// //         Animation<double> secondaryAnimation) {
+// //       return builder(context);
+// //     },
+// //     transitionsBuilder: (BuildContext context, Animation<double> animation,
+// //         Animation<double> secondaryAnimation, Widget child) {
+// //       return child;
+// //     },
+// //   );
+// // }

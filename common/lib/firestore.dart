2d739d50027965865849e8800d4888d9a0b2a83d
kovarny <sivochkin@gmail.com>
2023-04-20 07:59:09 +1000

    deleteCol

--- common/lib/firestore.dart
+++ common/lib/firestore.dart
@@ -1,9 +1,26 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-copyCollection(
+void copyCollection(
     CollectionReference source, CollectionReference destination) async {
   QuerySnapshot querySnapshot = await source.get();
   querySnapshot.docs.forEach((doc) async {
     await destination.doc(doc.id).set(doc.data());
   });
+}
+
+void deleteCol(CollectionReference ref) async {
+  var batch = FirebaseFirestore.instance.batch();
+  var counter = 0;
+
+  var c = await ref.get();
+  for (var l in c.docs) {
+    batch.delete(l.reference);
+    counter++;
+    if (counter > 490) {
+      await batch.commit();
+      batch = FirebaseFirestore.instance.batch();
+      counter = 0;
+    }
+  }
+  await batch.commit();
 }

9d98997b07fa9ed4be448b1474a85ce67ab7df26
kovarny <sivochkin@gmail.com>
2023-04-25 09:53:52 +1000

    doc field

--- widgets/lib/builders.dart
+++ widgets/lib/builders.dart
@@ -4,11 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:providers/firestore.dart';
 
-List<T> buildDocFieldList<T>(
-    BuildContext context, WidgetRef ref, String address,
+List<T> buildDocFieldList<T>(BuildContext context, WidgetRef ref, String path,
     {required T Function(BuildContext context, MapEntry<String, dynamic> data)
         builder}) {
-  return ref.watch(docSP(address)).when(
+  return ref.watch(docSP(path)).when(
       data: (doc) =>
           doc.data()!.entries.map<T>((e) => builder(context, e)).toList(),
       loading: () => [],
@@ -24,4 +23,14 @@ List<T> buildColItems<T>(
           colSnapshot.docs.map<T>((doc) => itemBuilder(context, doc)).toList(),
       loading: () => [],
       error: (e, s) => []);
+}
+
+T? buildDocField<T>(
+    BuildContext context, WidgetRef ref, String path, String field,
+    {required T Function(BuildContext context, MapEntry<String, dynamic> data)
+        builder}) {
+  return ref.watch(docSP(path)).when(
+      data: (doc) => doc.data()?[field] ?? null,
+      loading: () => null,
+      error: (e, s) => null);
 }

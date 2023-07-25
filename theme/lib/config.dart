6c9bb1bef2c630a7a35007d504cbbb6acbfb6f3b
AltanBaysal <96086932+AltanBaysal@users.noreply.github.com>
2023-07-26 02:54:57 +0300

    config and theme_mode updated

--- theme/lib/config.dart
+++ theme/lib/config.dart
@@ -1,21 +1,32 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
+/// true for light
 class ThemeModeConfig {
-  static bool enableSave = true;
-  static bool defaultToLightTheme = false;
-  static Function(bool themeMode) saveBuilder = (themeMode) => FirebaseFirestore
-      .instance
-      .collection('user')
-      .doc(FirebaseAuth.instance.currentUser!.uid)
-      .set({'themeMode': themeMode == false ? 'light' : 'dark'},
-          SetOptions(merge: true));
-  static Future<bool> Function() getBuilder = () async {
+  static bool defaultToLightTheme = true;
+
+  /// this function used for save themeData to firestore if any exception happens it returns false otherwise true
+  static Future<bool> saveTheme(bool themeData) async {
+    try {
+      await FirebaseFirestore.instance
+          .collection('user')
+          .doc(FirebaseAuth.instance.currentUser!.uid)
+          .set(
+        {'themeMode': themeData ? 'light' : 'dark'},
+        SetOptions(merge: true),
+      );
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  static Future<bool> getTheme() async {
     final theme = (await FirebaseFirestore.instance
             .collection('user')
             .doc(FirebaseAuth.instance.currentUser!.uid)
             .get())
         .data()!['themeMode'];
-    return theme == 'light' ? false : true;
-  };
+    return theme == 'light';
+  }
 }
